0cf9151d3d23d118ca5880e6966ff8bdad748e99
Owen Valentinus <owenwijaya89@gmail.com>
2022-09-07 23:22:25 +0800

    first init

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+urls = [
+    "https://edwag.github.io/js/cj5-tc.min.json",
+    "https://edwag.github.io/js/cj5-tc.min.jsfeon"
+]
+
+headers = {
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,id;q=0.8"
+}
+
+
+def fetch_response(url):
+    response = requests.get(url, headers=headers).json()
+    return response
+
+
+for index, url in enumerate(urls):
+    with open(f'./data/{index}.json', 'w', encoding='utf-8') as file:
+        response = fetch_response(url)
+        json.dump(response, file, ensure_ascii=False)
